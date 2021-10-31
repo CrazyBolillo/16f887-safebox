b9617eb98af95ed7550720adb34e43744abb10fd
CrazyBolillo <40015596+CrazyBolillo@users.noreply.github.com>
2021-10-31 11:42:30 -0600

    Implemented keypad_read()

--- keypad.h
+++ keypad.h
@@ -3,6 +3,17 @@
 
 #include "config.h"
 
+#define COL4 PORTBbits.RB0
+#define COL3 PORTBbits.RB1
+#define COL2 PORTBbits.RB2
+#define COL1 PORTBbits.RB1
+
+#define ROW4 PORTBbits.RB4
+#define ROW3 PORTBbits.RB5
+#define ROW2 PORTBbits.RB6
+#define ROW1 PORTBbits.RB7
+
 void keypad_init(void);
+char keypad_read(void);
 
 #endif
